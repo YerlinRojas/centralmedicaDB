7b2a4ed61e8a7fc7295fb073c74e9883d828661f
Yerlin ROJAS <yerlinrojas808@gmail.com>
2024-03-25 13:32:37 -0300

    actualizacion tablas

--- database/SQL/central_medica_db_rojas.sql
+++ database/SQL/central_medica_db_rojas.sql
@@ -5,9 +5,10 @@ USE centralmedical_db;
 
 
 CREATE TABLE patients(
-    id_patient INT  AUTO_INCREMENT PRIMARY KEY,
-    firts_name VARCHAR(100),
-    last_name VARCHAR(100),
+	id_patient INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     birth DATE,
     gender VARCHAR(10),
     address VARCHAR(255),
@@ -17,8 +18,9 @@ COMMENT "PATIENTS DATA"
 ;
 CREATE TABLE doctors(
     id_doctor INT AUTO_INCREMENT PRIMARY KEY,
-    firts_name VARCHAR(100),
-    last_name VARCHAR(100),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     speciality VARCHAR(100),
     phone VARCHAR(20),
     room VARCHAR(100)
@@ -27,24 +29,25 @@ COMMENT "DOCTORS DATA"
 ;
 
 CREATE TABLE appointments(
-    id_appointment INT AUTO_INCREMENT PRIMARY KEY,
+	id_appointment INT AUTO_INCREMENT PRIMARY KEY,
     id_patient INT,
     id_doctor INT,
     appointment_date DATE,
-    appointment_time TIME
+    appointment_time TIME,
+    status ENUM('Pending', 'Confirmed', 'Cancelled', 'Completed') DEFAULT 'Pending'
    )
    COMMENT "APPOINTMENTS DATA"
    ;
    
    
 CREATE TABLE availabilityDate (
-    id_availabilityDate INT AUTO_INCREMENT PRIMARY KEY,
+    id_availability_date INT AUTO_INCREMENT PRIMARY KEY,
     id_doctor INT,
     availability_date DATE,
     availability_time TIME,
-    status VARCHAR(20),
+    status ENUM('Available', 'Booked', 'Cancelled') DEFAULT 'Available',
     comment VARCHAR(255)
-)
+    )
 COMMENT "AVAILABILITY DATES"
 ;
 
@@ -54,7 +57,7 @@ CREATE TABLE medical_history (
     id_doctor INT,
     concurrence_date DATE,
     details TEXT,
-	diagnosis TEXT,
+    diagnosis TEXT,
     treatment TEXT
  )
  COMMENT "MEDICAL HISTORY"
@@ -62,10 +65,10 @@ CREATE TABLE medical_history (
 
 
 CREATE TABLE therapy(
-	id_sesion BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-    id_patient INT ,
+	id_session BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    id_patient INT,
     id_doctor INT,
-    sesion_date DATE,
+    session_date DATE,
     details TEXT,
     progress TEXT,
     obs TEXT
@@ -73,6 +76,14 @@ CREATE TABLE therapy(
     COMMENT "THERAPY DATA"
     ;
     
+-- PATIENTS
+ALTER TABLE patients
+ADD CONSTRAINT chk_gender CHECK (gender IN ('Male', 'Female', 'Other')),
+ADD CONSTRAINT chk_email_format_patients CHECK (email LIKE '%_@__%.__%');
+
+-- DOCTORS
+ALTER TABLE doctors 
+ADD CONSTRAINT chk_email_format_doctors CHECK (email LIKE '%_@__%.__%');
 
 
   -- APPOINMENTS  
@@ -90,7 +101,7 @@ ALTER TABLE appointments
 ADD COLUMN id_availability INT,
 ADD CONSTRAINT fk_availability_id
 FOREIGN KEY (id_availability)
-REFERENCES availabilityDate(id_availabilityDate);
+REFERENCES availabilityDate(id_availability_date);
 
 	
 
